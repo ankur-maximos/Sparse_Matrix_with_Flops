2927f22acae03cd7798c2d910e261d74d7872ad8
qingpeng niu <niuq@ri.cse.ohio-state.edu>
2013-11-10 11:41:06 -0500

    Remove array thresh prune test

--- tests/util_test.cc
+++ tests/util_test.cc
@@ -1,4 +1,4 @@
-#include "util.h"
+#include "tools/util.h"
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
@@ -28,28 +28,10 @@ void arrayInflationR2Test() {
   printf("%s Passed\n", __func__);
 }
 
-void arrayThreshPruneTest() {
-  const double thresh = 2.0;
-  double values[] = {2.1, 1.5, 4.0, 3.0, -2.0};
-  int count = sizeof(values) / sizeof(double);
-  int indices[] = {1, 2, 3, 4, 5};
-  arrayThreshPrune(thresh, indices, values,
-      &count, indices, values);
-  assert(fabs(values[0] - 2.1) <= 1.0e-7);
-  assert(fabs(values[1] - 4.0) <= 1.0e-7);
-  assert(fabs(values[2] - 3.0) <= 1.0e-7);
-  assert(count == 3);
-  assert(indices[0] == 1);
-  assert(indices[1] == 3);
-  assert(indices[2] == 4);
-  printf("%s Passed\n", __func__);
-}
-
 int main() {
   arrayMaxTest();
   arraySumTest();
   arrayInflationR2Test();
-  arrayThreshPruneTest();
   printf("Passed\n");
   return 0;
 }
