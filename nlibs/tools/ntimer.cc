d72601328d833d5f90b971439ad321636d4436b5
qingpeng niu <niuq@ri.cse.ohio-state.edu>
2014-01-13 17:19:21 -0500

    Fix bugs for timing with double 1000.0 instead of integer.

--- nlibs/tools/ntimer.cc
+++ nlibs/tools/ntimer.cc
@@ -4,6 +4,6 @@ double time_in_mill_now() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
   double time_in_mill =
-    (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+    (tv.tv_sec) * 1000.0 + (tv.tv_usec) / 1000.0;
   return time_in_mill;
 }
