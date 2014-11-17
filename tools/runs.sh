00de025ae7401b39915b282a96296cf7059881c7
Qingpeng Niu <niuqingpeng@gmail.com>
2014-11-17 17:08:13 -0500

    Makefile

--- tools/runs.sh
+++ tools/runs.sh
@@ -1,5 +1,5 @@
 dataDir=/home/niuq/data/matrix_input
-inputs="facebook_combined.txt email-Enron.txt com-dblp.ungraph.txt roadNet-CA.txt loc-gowalla_edges.txt com-youtube.ungraph.txt BioGRID_unweighted_graph.txt DIP_unweighted_graph.txt WIPHI_graph.txt com-amazon.ungraph.txt web-Google.txt"
+inputs="com-amazon.ungraph.txt email-Enron.txt roadNet-CA.txt  twitter_combined.txt web-NotreDame.txt BioGRID_unweighted_graph.txt  com-dblp.ungraph.txt  com-youtube.ungraph.txt facebook_combined.txt  pwtk.txt web-BerkStan.txt WIPHI_graph.txt cit-HepPh.txt DIP_unweighted_graph.txt  loc-gowalla_edges.txt  web-Google.txt com-lj.ungraph.txt cant.mtx  consph.mtx  cop20k_A.mtx  mac_econ_fwd500.mtx  mc2depi.mtx  pdb1HYS.mtx  pwtk.mtx  rail4284.mtx  rail4284.tar.gz  rma10.mtx  scircuit.mtx  webbase-1M.mtx"
 stride=512
 bin=analysis.x
 for input in $inputs; do
